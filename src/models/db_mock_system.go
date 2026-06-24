062cbfb20292bd2ef1bfd6cbeea02e4c05770d2d
阿宝 <346299437@qq.com>
2026-06-24 14:31:09 +0800

    整理项目go文件按模块命名

--- src/models/db_mock_system.go
+++ src/models/db_mock_system.go
@@ -16,10 +16,13 @@ func mockSystemData(sc *config.ServerConfig) *User {
 		{Name: "RoleManagement", Title: "角色管理", Icon: "ant-design:solution-outlined", Type: "1", Show: "1", OrderNo: 93, Component: "system/role/index", Pid: 1, Path: "role"},
 		{Name: "ChangePassword", Title: "修改密码", Icon: "ant-design:key-outlined", Type: "1", Show: "1", OrderNo: 94, Component: "system/password/index", Pid: 1, Path: "changePassword"},
 		{Name: "ApiManagement", Title: "接口授权", Icon: "ant-design:api-outlined", Type: "1", Show: "1", OrderNo: 95, Component: "system/api/index", Pid: 1, Path: "api"},
+
 		{Name: "PermissionManagement", Title: "权限管理", Icon: "ion:layers-outline", Type: "0", Show: "1", OrderNo: 100, Component: "LAYOUT", Redirect: "/permission/front/page", Path: "/permission"},
 		{Name: "PermissionFront", Title: "前端权限管理", Icon: "ion:layers-outline", Type: "1", Show: "1", OrderNo: 101, Component: "/permission/front/index", Pid: 7, Path: "front"},
-		{Name: "ServiceTree", Title: "服务树与CMDB", Icon: "ant-design:database-outlined", Type: "0", Show: "1", OrderNo: 10, Component: "LAYOUT", Path: "/serviceTree", Redirect: "/serviceTree/service/index"},
+
+		{Name: "ServiceTree", Title: "CMDB资产管理", Icon: "ant-design:database-outlined", Type: "0", Show: "1", OrderNo: 10, Component: "LAYOUT", Path: "/serviceTree", Redirect: "/serviceTree/service/index"},
 		{Name: "ServiceTreeIndexAsync", Title: "服务树", Icon: "ant-design:node-index-outlined", Type: "1", Show: "1", OrderNo: 11, Component: "stree/stree/indexAsync", Pid: 9, Path: "streeAsync"},
+
 		{Name: "WorkOrder", Title: "工单服务", Icon: "ant-design:reconciliation-outlined", Type: "0", Show: "1", OrderNo: 20, Component: "LAYOUT", Path: "/workOrder", Redirect: "/workOrder/process/index"},
 		{Name: "ProcessManagement", Title: "审批流程管理", Icon: "ant-design:apartment-outlined", Type: "1", Show: "1", OrderNo: 21, Component: "workorder/process/index", Pid: 11, Path: "process"},
 		{Name: "FormManagement", Title: "表单设计管理", Icon: "ant-design:form-outlined", Type: "1", Show: "1", OrderNo: 22, Component: "workorder/formDesign/index", Pid: 11, Path: "formDesign"},
@@ -27,6 +30,7 @@ func mockSystemData(sc *config.ServerConfig) *User {
 		{Name: "WorkOrderTicket", Title: "工单申请", Icon: "ant-design:profile-outlined", Type: "1", Show: "1", OrderNo: 24, Component: "workorder/ticket/index", Pid: 11, Path: "ticket"},
 		{Name: "WorkOrderCreate", Title: "工单填写", Icon: "ant-design:form-outlined", Type: "1", Show: "0", OrderNo: 25, Component: "workorder/ticket/create", Pid: 11, Path: "create"},
 		{Name: "WorkOrderSearch", Title: "我的工单", Icon: "ant-design:profile-outlined", Type: "1", Show: "1", OrderNo: 26, Component: "workorder/ticket/search", Pid: 11, Path: "search"},
+
 		{Name: "JobExec", Title: "任务执行中心", Icon: "ant-design:reconciliation-outlined", Type: "0", Show: "1", OrderNo: 30, Component: "LAYOUT", Path: "/JobExec", Redirect: "/JobExec/task/index"},
 		{Name: "JobExecScript", Title: "脚本管理", Icon: "ant-design:profile-outlined", Type: "1", Show: "1", OrderNo: 31, Component: "jobExec/script/index", Pid: 18, Path: "script"},
 		{Name: "JobExecTask", Title: "任务管理", Icon: "ant-design:profile-outlined", Type: "1", Show: "1", OrderNo: 32, Component: "jobExec/task/index", Pid: 18, Path: "task"},
